3687b77e3c64096e940f7ea138b8a24bfd0b98b4
nowgnas <marshmello.sw@gmail.com>
2023-01-17 07:54:59 +0900

    주문 상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -49,5 +49,47 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        // 생성 시에 createOrder를 호출 해서 여기서 완결한다
+        // 주문 생성에 관한건 여기만 고치면 된다
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem :
+                orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
 
+    // 비즈니스 로직 
+    public void cancel() {
+        if (delivery.getDeliveryStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송된 상품은 취소가 불가능합니다");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem :
+                orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+
+    /**
+     * 전체 주문 가격 조회
+     *
+     * @return
+     */
+    public int getTotalPrice() {
+        // stream으로 하면 간단하게 할 수 있다
+        int totalPrice = 0;
+        for (OrderItem orderitem : orderItems) {
+            totalPrice += orderitem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
