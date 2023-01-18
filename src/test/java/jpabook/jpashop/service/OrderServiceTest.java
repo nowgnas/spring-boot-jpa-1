cfb23315c3c3389043648c3c35c248f3cf1647a9
nowgnas <marshmello.sw@gmail.com>
2023-01-19 08:09:38 +0900

    주문 서비스 개발

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,75 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+
+        Item book = new Book();
+        book.setName("시골");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
+        // when
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        // then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        Assert.assertEquals("상품 주문 시 상태는 order", OrderStatus.ORDER, getOrder.getStatus());
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
